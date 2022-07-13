8a58dd2409eae6127176a7c07d39419e828829d8
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-07-13 11:03:54 +0200

    feat: icon component

---
+++ src/components/atoms/Icon.tsx
@@ -0,0 +1,30 @@
+import { IconNames } from "../../types/props";
+
+interface IconProps {
+  name: IconNames;
+  className?: string;
+  height?: number;
+  width?: number;
+  size?: number;
+  color?: string;
+  alt?: string;
+}
+
+export default function Icon({
+  name,
+  className = "",
+  size = 14,
+  height = size,
+  width = size,
+  alt = name + " icon",
+}: IconProps) {
+  return (
+    <img
+      src={`/assets/icons/${name}.svg`}
+      alt={alt}
+      width={width}
+      height={height}
+      className={className}
+    />
+  );
+}
