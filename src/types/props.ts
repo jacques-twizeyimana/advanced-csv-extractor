e1e4ad45683b69c46655a5c55d6cd5fcc6ff08fd
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-07-13 11:06:54 +0200

    feat: button component

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,1 +1,12 @@
 export type IconNames = "close";
+
+export type Color =
+  | "primary-400"
+  | "primary-500"
+  | "primary-600"
+  | "neutral-250"
+  | "neutral-350"
+  | "neutral-400"
+  | "neutral-500"
+  | "neutral-600"
+  | "error-500";
