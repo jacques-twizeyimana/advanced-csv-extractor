b81ea7c457bc35d715589ef41dd53b05db0f9984
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-07-15 15:58:31 +0200

    style: allow table to auto scroll horizontally

--- src/components/molecules/Table.tsx
+++ src/components/molecules/Table.tsx
@@ -12,29 +12,31 @@ export default function Table({
   startingRow = 0,
 }: ITableProps) {
   return (
-    <table className="w-full mt-5">
-      {showHeader && (
-        <thead>
-          <tr>
-            {Object.values(data[0]).map((key, index) => (
-              <th key={index} scope="col p-2 text-xs" className="border">
-                {key}
-              </th>
-            ))}
-          </tr>
-        </thead>
-      )}
-      <tbody>
-        {data.slice(1).map((row, index) => (
-          <tr key={index}>
-            {Object.values(row).map((value, index) => (
-              <td scope="col" className="border px-2 text-sm" key={index}>
-                {value}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="overflow-x-auto">
+      <table className="w-full mt-5">
+        {showHeader && (
+          <thead>
+            <tr>
+              {Object.values(data[0]).map((key, index) => (
+                <th key={index} scope="col p-2 text-xs" className="border">
+                  {key}
+                </th>
+              ))}
+            </tr>
+          </thead>
+        )}
+        <tbody>
+          {data.slice(1).map((row, index) => (
+            <tr key={index}>
+              {Object.values(row).map((value, index) => (
+                <td scope="col" className="border px-2 text-sm" key={index}>
+                  {value}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
