3cdf12ddd7d4771f66474dad20f9c031bff5b23e
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-07-15 15:55:26 +0200

    feat: show all columns

--- src/components/molecules/Table.tsx
+++ src/components/molecules/Table.tsx
@@ -3,15 +3,20 @@ import { IData } from "../../types/props";
 interface ITableProps {
   data: IData[];
   showHeader?: boolean;
+  startingRow?: number;
 }
 
-export default function Table({ data, showHeader = true }: ITableProps) {
+export default function Table({
+  data,
+  showHeader = true,
+  startingRow = 0,
+}: ITableProps) {
   return (
     <table className="w-full mt-5">
       {showHeader && (
         <thead>
           <tr>
-            {Object.keys(data[0]).map((key, index) => (
+            {Object.values(data[0]).map((key, index) => (
               <th key={index} scope="col p-2 text-xs" className="border">
                 {key}
               </th>
@@ -20,7 +25,7 @@ export default function Table({ data, showHeader = true }: ITableProps) {
         </thead>
       )}
       <tbody>
-        {data.map((row, index) => (
+        {data.slice(1).map((row, index) => (
           <tr key={index}>
             {Object.values(row).map((value, index) => (
               <td scope="col" className="border px-2 text-sm" key={index}>
