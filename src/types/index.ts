322460aed078c62a50dbfa7842570678f5723d73
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-07-17 22:38:55 +0200

    feat: custom multiselect

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+interface Islurry {
+  data: Array<Number>;
+  unit: string;
+}
+
+export interface ISubmitInfo {
+  stage: number;
+  well: number;
+  offset_min: number;
+  time: Array<Array<Number>>;
+  slurry: Islurry;
+  pressure: Islurry;
+  prop: Islurry;
+}
+
+/*
+'{"stage": 42, "well": 43, "offset_min": 0, "time": [[1, 2, 3], [4, 5, 6]], "slurry": {"data": [1, 2, 3], "unit": "bpm"}, "pressure": {"data": [1, 2, 3], "unit": "psi"},
+ "prop": {"data": [1, 2, 3], "unit": "ppg"}}'
+*/
--- src/types/props.ts
+++ src/types/props.ts
@@ -1,4 +1,11 @@
-export type IconNames = "close" | "upload" | "table" | "process";
+import { FormEvent } from "react";
+
+export type IconNames =
+  | "close"
+  | "upload"
+  | "table"
+  | "process"
+  | "chevron-down";
 
 export type Color =
   | "primary-400"
@@ -11,6 +18,36 @@ export type Color =
   | "neutral-600"
   | "error-500";
 
-  export interface IData {
-    [key: string]: string | number | boolean;
-  }
+export interface IData {
+  [key: string]: string | number | boolean;
+}
+
+export interface SelectData {
+  value: string | number;
+  label: string;
+  subLabel?: string;
+}
+
+export interface ValueType<T = Event> {
+  name: string;
+  value: string | number | boolean | string[];
+  label?: string;
+  event?: FormEvent<T>;
+}
+
+export interface commonInputProps {
+  required?: boolean;
+  handleChange: (_e: ValueType) => any;
+  name: string;
+  options: SelectData[];
+  className?: string;
+  placeholder?: string;
+  disabled?: boolean;
+}
+
+export interface MultiselectProps extends commonInputProps {
+  loading?: boolean;
+  value?: string[];
+  hasError?: boolean;
+  width?: string;
+}
