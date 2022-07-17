73b73e3edfc6fa8044b8f94179134463da86dba9
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-07-17 23:45:29 +0200

    feat: add more options

--- src/components/molecules/Import.tsx
+++ src/components/molecules/Import.tsx
@@ -1,10 +1,12 @@
 import { FormEvent, useState } from "react";
-import { IconNames, IData } from "../../types/props";
+import { IconNames, IData, ValueType } from "../../types/props";
 import FilePicker from "../atoms/FilePicker";
 import Icon from "../atoms/Icon";
 import Papa from "papaparse";
 import Table from "./Table";
 import { ISubmitInfo } from "../../types";
+import Multiselect from "./Multiselect";
+import Select from "./Select";
 
 export default function Import() {
   const [step, setStep] = useState(0);
@@ -17,17 +19,18 @@ export default function Import() {
 
   const [values, setValues] = useState({
     pressure: [],
-    slurry_rate: "",
-    prop_conc: "",
-    unitOfPressure: "",
+    slurry: [],
+    time: [],
+    unitOfSlurry: "bpm",
+    unitOfPressure: "psi",
+    propConc: [],
+    unitOfPropConc: "ppg",
     separator: ",",
     numHeaderRows: 2,
   });
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>
-  ) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
+  const handleChange = (e: ValueType) => {
+    setValues({ ...values, [e.name]: e.value });
   };
 
   const handleParse = () => {
@@ -68,22 +71,32 @@ export default function Import() {
       offset_min: 1,
       time: [[]],
       slurry: {
-        data: [],
-        unit: "",
+        data: values.slurry,
+        unit: values.unitOfSlurry,
       },
       pressure: {
         data: values.pressure,
         unit: values.unitOfPressure,
       },
       prop: {
-        data: [],
-        unit: "",
+        data: values.propConc,
+        unit: values.unitOfPropConc,
       },
     };
   };
 
+  const iMissingData =
+    !values.pressure ||
+    !values.slurry ||
+    !values.propConc ||
+    !values.unitOfPressure ||
+    !values.unitOfSlurry ||
+    !values.unitOfPropConc ||
+    !values.separator ||
+    !values.numHeaderRows;
+
   return (
-    <div className="">
+    <div>
       <div className="flex items-center justify-between gap-6 pb-6 border-b">
         {steps.map((icon, index) => (
           <Icon
@@ -96,119 +109,162 @@ export default function Import() {
           />
         ))}
       </div>
-      <div className="py-6">
-        {step === 0 ? (
-          <div>
-            <p className="text-base pb-2">Import CSV file</p>
-            <div className="w-full">
-              <FilePicker
-                placeholder={"No file selected"}
-                hasError={hasError}
-                handleUpload={(file) => setFile(file)}
-                //  handleUpload={handleParse}
-                accept=".csv"
-              />
-            </div>
-            <div className="pt-6">
-              <button
-                className={`${
-                  !file ? "bg-neutral-400" : "bg-neutral-800"
-                } text-white font-bold py-3 px-6 rounded-lg`}
-                disabled={!file}
-                onClick={handleParse}
-              >
-                Process data
-              </button>
-            </div>
+      {step === 0 ? (
+        <div className="import-file">
+          <p className="text-base pb-2">Import CSV file</p>
+          <div className="w-full">
+            <FilePicker
+              placeholder={"No file selected"}
+              hasError={hasError}
+              handleUpload={(file) => setFile(file)}
+              //  handleUpload={handleParse}
+              accept=".csv"
+            />
           </div>
-        ) : step === 1 ? (
-          <div>
-            <div className="pb-5 max-w-4xl">
-              <h2 className="text-xl font-bold">Define input data</h2>
-              <div className="grid grid-cols-2 gap-6 py-5">
-                <div>
-                  <div className="py-2">
-                    <label>pressure</label>
-                    <select
-                      className="w-full py-2 px-3 rounded-md text-sm"
+          <div className="pt-6">
+            <button
+              className={`${
+                !file ? "bg-neutral-400" : "bg-neutral-800"
+              } text-white font-bold py-3 px-6 rounded-lg`}
+              disabled={!file}
+              onClick={handleParse}
+            >
+              Process data
+            </button>
+          </div>
+        </div>
+      ) : step === 1 ? (
+        <div className="choose-options">
+          <div className="py-10  md:w-10/12 lg:w-2/3">
+            <h2 className="text-xl font-bold">Define input data</h2>
+            {/* trick JIT engine */}
+            <div className="w-20 hidden" />
+            <div className="grid grid-cols-2 gap-6 py-5">
+              <div className="left-selects">
+                <div className="py-2">
+                  <label>pressure</label>
+                  <div className="py-2 flex gap-1">
+                    <Multiselect
                       name="pressure"
-                      onChange={handleChange}
-                    >
-                      {Object.values(first40[0]).map((key, index) => (
-                        <option value={key.toString()} key={index}>
-                          {key} (col {index + 1})
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <div className="py-2">
-                    <label>Slurry rate</label>
-                    <select
-                      className="w-full py-2 px-3 rounded-md text-sm"
-                      name="slurry_rate"
-                      onChange={handleChange}
-                    >
-                      {Object.values(first40[0]).map((key, index) => (
-                        <option value={key.toString()} key={index}>
-                          {key} (col {index + 1})
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <div className="py-2">
-                    <label>pressure unit of measurement</label>
-                    <select
-                      className="w-full py-2 px-3 rounded-md text-sm"
-                      name="unitOfMeasurement"
-                      onChange={handleChange}
-                    >
-                      <option value={"ft"}>Feet</option>
-                      <option value={"m"}>Meter</option>
-                      <option value={"cm"}>Centimeter</option>
-                      <option value={"km"}>Kilometer</option>
-                    </select>
+                      value={values.pressure}
+                      handleChange={handleChange}
+                      options={Object.values(first40[0]).map((key, index) => ({
+                        label: `${key} - (col ${index + 1})`,
+                        value: index,
+                      }))}
+                    />
+
+                    <Select
+                      name="unitOfPressure"
+                      handleChange={handleChange}
+                      value={values.unitOfPressure}
+                      width={"20"}
+                      options={[
+                        { label: "pa", value: "pascal" },
+                        { label: "psi", value: "psi" },
+                      ]}
+                    />
                   </div>
                 </div>
-                <div>
-                  <div className="py-2">
-                    <label>Decimal separator</label>
-                    <select
-                      className="w-full py-2 px-3 rounded-md text-sm"
-                      name="separator"
-                      onChange={handleChange}
-                    >
-                      <option value=".">Dot</option>
-                      <option value=",">Comma</option>
-                    </select>
-                  </div>
+                <div className="py-2">
+                  <label>Slurry rate</label>
+                  <div className="py-2 flex gap-1">
+                    <Multiselect
+                      name="slurry"
+                      value={values.slurry}
+                      handleChange={handleChange}
+                      options={Object.values(first40[0]).map((key, index) => ({
+                        label: `${key} - (col ${index + 1})`,
+                        value: index,
+                      }))}
+                    />
 
-                  <div className="py-2">
-                    <label>Prop conc</label>
-                    <select
-                      className="w-full py-2 px-3 rounded-md text-sm"
-                      name="prop_conc"
-                      onChange={handleChange}
-                    >
-                      {Object.values(first40[0]).map((key, index) => (
-                        <option value={key.toString()} key={index}>
-                          {key} (col {index + 1})
-                        </option>
-                      ))}
-                    </select>
+                    <Select
+                      name="unitOfSlurry"
+                      handleChange={handleChange}
+                      value={values.unitOfSlurry}
+                      width={"20"}
+                      options={[
+                        { label: "bpm", value: "bpm" },
+                        { label: "m3/s", value: "m3/s" },
+                      ]}
+                    />
                   </div>
-                  <div className="py-2">
-                    <label>Number of header rows</label>
-                    <input
-                      className="w-full py-2 px-3 rounded-md text-sm border border-gray-500"
-                      type="number"
-                      name="numHeaderRows"
-                      onChange={handleChange}
-                      value={values.numHeaderRows}
+                </div>
+                <div className="py-2 ">
+                  <label>Time column</label>
+                  <div className="flex py-2">
+                    <Multiselect
+                      name="time"
+                      handleChange={handleChange}
+                      value={values.time}
+                      options={Object.values(first40[0]).map((key, index) => ({
+                        label: `${key} - (col ${index + 1})`,
+                        value: index,
+                      }))}
                     />
                   </div>
                 </div>
               </div>
+              <div className="right-selects">
+                <div className="py-2">
+                  <label>Pronc conc</label>
+                  <div className="py-2 flex gap-1">
+                    <Multiselect
+                      name="propConc"
+                      value={values.propConc}
+                      handleChange={handleChange}
+                      options={Object.values(first40[0]).map((key, index) => ({
+                        label: `${key} - (col ${index + 1})`,
+                        value: index,
+                      }))}
+                    />
+
+                    <Select
+                      name="unitOfPropConc"
+                      handleChange={handleChange}
+                      value={values.unitOfPropConc}
+                      width={"20"}
+                      options={[
+                        { label: "ppg", value: "ppg" },
+                        { label: "kg/m3", value: "kg/m3" },
+                      ]}
+                    />
+                  </div>
+                </div>
+                <div className="py-2">
+                  <label>Decimal separator</label>
+                  <div className="py-2 flex">
+                    <Select
+                      name="separator"
+                      handleChange={handleChange}
+                      value={values.separator}
+                      options={[
+                        { label: "dot", value: "." },
+                        { label: "comma", value: "," },
+                      ]}
+                    />
+                  </div>
+                </div>
+                <div className="py-2">
+                  <label>Number of header rows</label>
+                  <input
+                    className="w-full py-2 px-3 rounded-md text-sm border border-gray-500"
+                    type="number"
+                    name="numHeaderRows"
+                    value={values.numHeaderRows}
+                    onChange={(e) =>
+                      setValues({
+                        ...values,
+                        numHeaderRows: parseInt(e.target.value),
+                      })
+                    }
+                  />
+                </div>
+              </div>
             </div>
+          </div>
+          <div className="table-data py-2">
             <h2 className="text-xl font-bold">Preview</h2>
             <Table data={first40} />
             <Table
@@ -217,17 +273,20 @@ export default function Import() {
               showHeader={false}
             />
           </div>
-        ) : step === 2 ? (
-          <div>
-            <p className="text-base pb-2">Import</p>
-            <div className="w-full">
-              <button className="bg-primary-500 text-white px-4 py-2 rounded-lg">
-                Import
-              </button>
-            </div>
+          <div className="py-12">
+            <button
+              className={`${
+                iMissingData ? "bg-neutral-400" : "bg-neutral-800"
+              } text-white font-medium py-3 px-6 rounded-lg`}
+              disabled={iMissingData}
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Upload to server
+            </button>
           </div>
-        ) : null}
-      </div>
+        </div>
+      ) : null}
     </div>
   );
 }
